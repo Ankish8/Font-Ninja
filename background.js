34d0ed48f57adca6940625353cd7638bda0aba25
Ankish8 <hsiknv9@gmail.com>
2024-02-18 13:27:17 +0530

    Toast working

--- background.js
+++ background.js
@@ -4,6 +4,9 @@ chrome.action.onClicked.addListener((tab) => {
     extensionEnabled = !extensionEnabled;
     chrome.storage.local.set({ extensionEnabled: extensionEnabled });
 
+    // Send a message to show/hide the toast notification based on extensionEnabled
+    chrome.tabs.sendMessage(tab.id, { showNotification: extensionEnabled });
+
     let iconPath = extensionEnabled ? 'images/icon16.png' : 'images/icon_disabled16.png';
     chrome.action.setIcon({ path: iconPath, tabId: tab.id });
 });
